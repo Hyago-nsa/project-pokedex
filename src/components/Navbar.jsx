2742d47666be6f6e48ece8f81c4c789d7f7e9068
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-01-10 17:31:05 -0300

    Add Logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img
+        className="logo"
+        alt="PokéAPI"
+        src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
