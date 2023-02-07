125f9a5212ac39a4c31707a5811029e19a7e677f
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-02-07 17:26:03 -0300

    deploying website

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://hyago-nsa.github.io/project-pokedex ",
   plugins: [react()],
   server: {
     host: true,
